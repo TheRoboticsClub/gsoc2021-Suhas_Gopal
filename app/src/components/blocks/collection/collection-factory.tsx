b6c28b660277e440d5a5ba805cf06c7b840a70b2
Suhas-G <suhas.g96.sg@gmail.com>
2021-07-07 10:54:22 +0530

    feat: opencv blocks

--- app/src/components/blocks/collection/collection-factory.tsx
+++ app/src/components/blocks/collection/collection-factory.tsx
@@ -12,8 +12,16 @@ export const collectionBlocks: { 'blocks': CollectionBlockType } = {
             'children': {
                 'blur': { 'label': 'Blur' },
                 'camera': { 'label': 'Camera' },
-                'screen': { 'label': 'Screen' },
+                'colorFilter': {'label': 'Color Filter'},
+                'contourDetector': {'label': 'Contour Detector'},
+                'cropper': {'label': 'Cropper'},
+                'dilation': {'label': 'Dilation'},
                 'edgeDetector': {'label': 'Edge Detector'},
+                'erosion': {'label': 'Erosion'},
+                'faceDetector': {'label': 'Face Detector'},
+                'imageRead': {'label': 'Image Read'},
+                'screen': { 'label': 'Screen' },
+                'threshold': {'label': 'Threshold'},
                 'videoStreamer': {'label': 'Video Streamer'}
             }
         },
@@ -34,10 +42,26 @@ export function getCollectionBlock(name: string) {
             return import('./opencv/Blur.json');
         case 'blocks.opencv.camera':
             return import('./opencv/Camera.json');
-        case 'blocks.opencv.screen':
-            return import('./opencv/Screen.json');
+        case 'blocks.opencv.colorFilter':
+            return import('./opencv/ColorFilter.json');
+        case 'blocks.opencv.contourDetector':
+            return import('./opencv/ContourDetector.json');
+        case 'blocks.opencv.cropper':
+            return import('./opencv/Cropper.json');
+        case 'blocks.opencv.dilation':
+            return import('./opencv/Dilation.json');
         case 'blocks.opencv.edgeDetector':
             return import('./opencv/EdgeDetector.json');
+        case 'blocks.opencv.erosion':
+            return import('./opencv/Erosion.json');
+        case 'blocks.opencv.faceDetector':
+            return import('./opencv/FaceDetector.json');
+        case 'blocks.opencv.imageRead':
+            return import('./opencv/ImageRead.json');
+        case 'blocks.opencv.screen':
+            return import('./opencv/Screen.json');
+        case 'blocks.opencv.threshold':
+            return import('./opencv/Threshold.json');
         case 'blocks.opencv.videoStreamer':
             return import('./opencv/VideoStreamer.json');
         case 'blocks.tensorflow.objectDetector':
